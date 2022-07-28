e1ec9c9250de7e4d6eaf117a6e4cf2813ae26364
Siyu Wang <wangxsiyu@gmail.com>
2022-07-28 11:12:37 -0400

    dqn draft

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,29 +1,117 @@
+import torch
 import torch.nn as nn
+import torch.optim as optim
+# from dqn.replay import ReplayMemory
+# from dqn.replay import Transition
+from itertools import count
+import numpy as np
+# import torch.nn.functional as F
+from collections import namedtuple, deque
+import random
 
-class DQN(nn.Module):
-    def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN_network(nn.Module):
+    def __init__(self, n_obs, n_action):
+        super(DQN_network, self).__init__()
+        self.dqn = nn.Sequential(
+            nn.Linear(n_obs, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, n_action)
+        )
+
     def forward(self, x):
-        x = x.to(device)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        return self.dqn(x)
+
+class DQN_agent():
+    def __init__(self, env):
+        self.env = env
+        self.dqn_policy = DQN_network(2, env.num_actions())
+        self.dqn_target = DQN_network(2, env.num_actions())
+        self.dqn_target.load_state_dict(self.dqn_policy.state_dict())
+        self.memory = ReplayMemory(10000)
+        self.batch_size = 32
+        self.target_update = 10
+        self.gamma = 0.999
+        self.optimizer = optim.Adam(self.dqn_policy.parameters())
+
+        self.device = "cpu" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def choose_action(self, obs):
+        return torch.randint(0, 2, (1,), device = self.device)
+
+    def train(self, num_episodes = 1):
+        env = self.env
+        for i_episode in range(num_episodes):
+            obs = env.reset()
+            for t in count():
+                # Select and perform an action
+                action = self.choose_action(obs)
+                obs_next, reward, done, _ = env.step(action.item())
+                reward = torch.tensor([reward], device = self.device)
+                # Store the transition in memory
+                self.memory.push(obs, action, obs_next, reward)
+                # Move to the next state
+                obs = obs_next
+                # Perform one step of the optimization (on the policy network)
+                self.step_train()
+                if done:
+                    break
+            # Update the target network, copying all weights and biases in DQN
+            if i_episode % self.target_update == 0:
+                self.dqn_target.load_state_dict(self.dqn_policy.state_dict())
+        print('Complete')
+
+    def step_train(self):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = self.memory.sample(self.batch_size)
+        batch = Transition(*zip(*transitions))
+        
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+            batch.next_state)), device = self.device, dtype = torch.bool)
+        non_final_next_states = torch.tensor([s for s in batch.next_state
+            if s is not None], device = self.device, dtype = torch.float)
+        state_batch = torch.tensor(batch.state, device = self.device, dtype = torch.float)
+        action_batch = torch.tensor(batch.action, device  = self.device)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        v_lastlayer = self.dqn_policy(state_batch)
+        state_action_values = torch.gather(v_lastlayer, dim = 1, index = action_batch.view(-1,1))
+
+        next_state_values = torch.zeros(self.batch_size, device = self.device)
+        next_state_values[non_final_mask] = self.dqn_target(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.dqn_policy.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
--- main_train.py
+++ main_train.py
@@ -1,10 +1,11 @@
+from dqn.dqn import DQN_agent
 from func_visualize import visualize_env
 from gym_whackamole_simple.envs import WhackAMole2
 import func_visualize
 
-env = WhackAMole2(render_mode="human", version = "full", render_fps = 10)
+env = WhackAMole2(render_mode=None, version = "full", render_fps = 10)
 params = env.params
-print(params)
+# print(params)
 params['gaze']['radius'] = 100
 params['gaze']['version_canmove'] = 0
 params['gaze']['version_resample']['cond'] = "fixed"
@@ -16,6 +17,11 @@ params['mole']['max_life'] = 1000
 params['reward_distance'] = 0
 params['reward_rotation'] = 10
 env.set_params(params)
+# print(f"num of actions {env.num_actions()}")
+# visualize_env(env)
 
-print(f"num of actions {env.num_actions()}")
-visualize_env(env)
+dqn = DQN_agent(env)
+dqn.train()
+
+
+
